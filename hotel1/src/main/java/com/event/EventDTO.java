60bb82849d911125c42bdeaffb10d3af5ed51402
1119j <1119j@DESKTOP-8G7GFGO>
2021-04-26 17:26:55 +0900

    PackDTO, EventDTO 작성

--- hotel1/src/main/java/com/event/EventDTO.java
+++ hotel1/src/main/java/com/event/EventDTO.java
@@ -1,5 +1,61 @@
 package com.event;
 
 public class EventDTO {
-
+	private int eventNum;
+	private String eventName;
+	private String subject;
+	private String content;
+	private String startDate;
+	private String endDate;
+	private String summery;
+	private String imageFilename;
+	
+	public int getEventNum() {
+		return eventNum;
+	}
+	public void setEventNum(int eventNum) {
+		this.eventNum = eventNum;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getSummery() {
+		return summery;
+	}
+	public void setSummery(String summery) {
+		this.summery = summery;
+	}
+	public String getImageFilename() {
+		return imageFilename;
+	}
+	public void setImageFilename(String imageFilename) {
+		this.imageFilename = imageFilename;
+	}
 }
--- hotel1/src/main/java/com/pack/PackDTO.java
+++ hotel1/src/main/java/com/pack/PackDTO.java
@@ -1,5 +1,68 @@
 package com.pack;
 
 public class PackDTO {
- 
+	private int pkgNum;
+	private String pkgName;
+	private String subject;
+	private String content;
+	private String startDate;
+	private String endDate;
+	private String discountPrice;
+	private String summery;
+	private String imageFilename;
+	
+	public int getPkgNum() {
+		return pkgNum;
+	}
+	public void setPkgNum(int pkgNum) {
+		this.pkgNum = pkgNum;
+	}
+	public String getPkgName() {
+		return pkgName;
+	}
+	public void setPkgName(String pkgName) {
+		this.pkgName = pkgName;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getDiscountPrice() {
+		return discountPrice;
+	}
+	public void setDiscountPrice(String discountPrice) {
+		this.discountPrice = discountPrice;
+	}
+	public String getSummery() {
+		return summery;
+	}
+	public void setSummery(String summery) {
+		this.summery = summery;
+	}
+	public String getImageFilename() {
+		return imageFilename;
+	}
+	public void setImageFilename(String imageFilename) {
+		this.imageFilename = imageFilename;
+	}
 }
