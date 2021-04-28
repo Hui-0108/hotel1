7bd076aca25ac104425769b270db1eb1665378ff
user <user@user-PC>
2021-04-28 17:22:29 +0900

    fnqDTO 작성

--- hotel1/src/main/java/com/faq/FnqDAO.java
+++ hotel1/src/main/java/com/faq/FnqDAO.java
@@ -1,5 +1,367 @@
 package com.faq;
 
-public class FnqDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.util.DBConn;
+
+public class FnqDAO {
+	private Connection conn = DBConn.getConnection();
+	
+	public int writeFnq(FnqDTO dto) throws SQLException {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+		
+		try {
+			sql = "INSERT INTO faq_bbs(num, subject, content, hitCount) "
+					+ " VALUES(faq_seq.NEXTVAL, ?, ?, ?, 0)";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dto.getSubject());
+			pstmt.setString(2, dto.getContent());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		return result;
+	}
+	public int dataCount() {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "SELECT COUNT(*) FROM bbs";
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	public List<FnqDTO> listBoard(int offset, int rows) {
+		List<FnqDTO> list = new ArrayList<FnqDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql="SELECT num, userName, subject, hitCount, TO_CHAR(created, 'YYYY-MM-DD') created"
+				+ "  FROM  bbs  b"
+				+ "  JOIN member1 m ON b.userId = m.userId"
+				+ "  ORDER BY num DESC"
+				+ "  OFFSET  ?  ROWS  FETCH  FIRST  ?  ROWS  ONLY";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, offset);
+			pstmt.setInt(2, rows);
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				FnqDTO dto = new FnqDTO();
+				dto.setNum(rs.getInt("num"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				
+				list.add(dto);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return list;
+	}
+	
+	public int dataCount(String condition, String keyword) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "SELECT COUNT(*) FROM bbs b JOIN member1 m ON b.userId = m.userId ";
+			if(condition.equals("all")) {
+				sql += "  WHERE INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ";
+			} else if(condition.equals("created")) {
+				keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+				sql += " WHERE TO_CHAR(created, 'YYYYMMDD') = ? ";
+			} else {
+				sql += " WHERE INSTR(" + condition + ", ?) >= 1 ";
+			}
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, keyword);
+			if(condition.equals("all")) {
+				pstmt.setString(2, keyword);
+			}
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = rs.getInt(1);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	public List<FnqDTO> listBoard(int offset, int rows, String condition, String keyword) {
+		List<FnqDTO> list = new ArrayList<FnqDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql="SELECT num, userName, subject, hitCount, TO_CHAR(created, 'YYYY-MM-DD') created"
+				+ "  FROM  bbs  b"
+				+ "  JOIN member1 m ON b.userId = m.userId";
+			if(condition.equals("all")) {
+				sql += "  WHERE INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ";
+			} else if(condition.equals("created")) {
+				keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+				sql += "  WHERE TO_CHAR(created, 'YYYYMMDD') = ? ";
+			} else {
+				sql += "  WHERE  INSTR(" + condition + ", ?) >= 1";
+			}
+			sql += "  ORDER BY num DESC"
+				 + "  OFFSET  ?  ROWS  FETCH  FIRST  ?  ROWS  ONLY";
+			
+				pstmt = conn.prepareStatement(sql);
+				
+				if(condition.equals("all")) {
+					pstmt.setString(1, keyword);
+					pstmt.setString(2, keyword);
+					pstmt.setInt(3, offset);
+					pstmt.setInt(4, rows);
+				} else {
+					pstmt.setString(1, keyword);
+					pstmt.setInt(2, offset);
+					pstmt.setInt(3, rows);
+				}
+				
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+					FnqDTO dto = new FnqDTO();
+					dto.setNum(rs.getInt("num"));
+					dto.setSubject(rs.getString("subject"));
+					dto.setHitCount(rs.getInt("hitCount"));
+					
+					list.add(dto);
+				}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return list;
+	}
+	
+	public int updateHitCount(int num) throws SQLException {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+		
+		try {
+			sql = "UPDATE bbs SET hitCount = hitCount + 1 WHERE num = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	public FnqDTO readBoard(int num) {
+		FnqDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "SELECT num, b.userId, userName, subject, content, hitCount, created "
+				+ "  FROM bbs b "
+				+ "  JOIN member1 m ON b.userId = m.userId "
+				+ "  WHERE num = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				dto = new FnqDTO();
+				dto.setNum(rs.getInt("num"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setContent(rs.getString("content"));
+				dto.setHitCount(rs.getInt("hitCount"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+			
+		}
+		
+		return dto;
+	}
+	public int updateBoard(FnqDTO dto) throws SQLException {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+		
+		try {
+			sql = "UPDATE bbs SET subject=?, content=? WHERE num=? AND userId=?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dto.getSubject());
+			pstmt.setString(2, dto.getContent());
+			pstmt.setInt(3, dto.getNum());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+					
+				}
+			}
+		}
+		return result;
+	}
+	
+	public int deleteBoard(int num, String userId) throws SQLException {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+		
+		try {
+			sql = "DELETE FROM bbs WHERE num = ?";
+			if(! userId.equals("admin")) {
+				sql += " AND userId = ?";
+			}
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			if(! userId.equals("admin")) {
+				pstmt.setString(2, userId);
+			}
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		return result;
+	}
 }
